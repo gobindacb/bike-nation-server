6607e9d9e88e526ac18ee7b9cb18b2da94eea009
gobindacb <p.gobinda.cb@gmail.com>
2022-11-30 23:06:15 +0600

    category collection

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(express.json())
 
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qt43ax4.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
