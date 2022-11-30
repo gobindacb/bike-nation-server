d9c5415b61c276b21c1d6fce12427bd5c01ae5ec
gobindacb <p.gobinda.cb@gmail.com>
2022-11-30 23:15:58 +0600

    save for final

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qt43ax4.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
